ff3296abdf0c53857e9084f3fda17d35edd6413c
Eli Dickinson <eli@industrydive.com>
2017-03-17 16:00:55 -0400

    hacky script to convert jsonlines to django fixture

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ $ scrapy crawl mm --set=FEED_URI=`pwd`/mm_data.jl --set=FEED_FORMAT=jsonlines
 
 Press Ctrl-C twice to abort
 
+Then you can convert it to a fixture for the legacy apps with this hacky script
+
+$ python mm_jsonlines_to_fixture.py
+
 
 ## how to deploy to scrapinghub
 
---
+++ mm_jsonlines_to_fixture.py
@@ -0,0 +1,20 @@
+# converts jsonlines in mm_data.jl to fixture similar to 
+# https://github.com/industrydive/divesite/blob/dea147db2a308dd3e0ce0ddaa65f824f0e201578/fixtures/mobile_marketer_test_data_tech-1167.json
+import json
+output = []
+with open('mm_data.jl') as f:
+    for line in f:
+        json_line = json.loads(line)
+        item = {
+            'model': 'legacyapps.MobileMarketerStory',
+            'fields': {
+                'site_id': 18,
+                'legacy_url_primary_path': json_line['url'],
+                'custom_byline':  ', '.join(json_line['authors']),
+                'title': json_line['title'],
+                'body': json_line['body'],
+            }
+        }
+        output.append(item)
+with open('mm_fixture.json', 'w') as outfile:
+    json.dump(output, outfile, indent=2)
